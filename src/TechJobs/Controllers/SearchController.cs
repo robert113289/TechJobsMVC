8306836a014ca7e91f1945f34913ecd2c9c40702
Robert Kirkum <robert113289@gmail.com>
2017-11-16 17:06:18 -0600

    resolved seach by all bug

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -18,7 +18,15 @@ namespace TechJobs.Controllers
 
         public IActionResult Results(string searchType, string searchTerm)
         {
-            List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+            if (searchType.Equals("all"))
+            {
+                 jobs = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                 jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
             ViewBag.jobs = jobs;
             ViewBag.columns = ListController.columnChoices;
 
